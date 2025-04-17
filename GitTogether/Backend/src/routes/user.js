6690b3f10d2d3a08286d88c2b2135d29d523cd10
Devesh Sharma <deveshsharma372002@gmail.com>
2025-04-17 15:54:09 +0530

    Added feed api

--- GitTogether/Backend/src/routes/user.js
+++ GitTogether/Backend/src/routes/user.js
@@ -30,12 +30,51 @@ userRouter.get("/user/connections", async (req, res) => {
         const loggedInUser = req.user;
         const connections = await connectionRequest.find({ $or : [{fromUserId : loggedInUser._id,status: "accepted"},{toUserId : loggedInUser._id,status : "accepted"}]} ).populate("fromUserId",["firstName","lastName","photoUrl","gender","age","skills"]);
         if (!connections) throw new Error("No Connections found");
-        const data = connections.map(row => row.fromUserId)
+        const data = connections.map(row =>{
+            if(row.toUserId===loggedInUser._id){
+                return row.fromUserId;
+            }else{
+                return row.toUserId;
+            }
+        })
         res.json({data : data});
 
     } catch (err) {
         res.status(400).send({ err: err.message })
 
+    }
+})
+
+
+userRouter.get("/user/feed",async(req,res)=>{
+    try {
+        
+        const loggedInUser = req.user;
+        //people i dont want in my feed
+        const page =parseInt( req.query.page) || 1;
+        let limit = parseInt(req.query.limit )|| 3;
+        limit = limit>50 ? 50: limit;
+        const skip = (page-1)*limit
+        const connections = await connectionRequest.find({ $or:[{fromUserId : loggedInUser._id},{toUserId : loggedInUser._id}]}).select("fromUserId toUserId")
+        // res.json({connections : connections})
+
+        const hideUserFromFeed = new Set();
+        connections.forEach(req =>{
+            hideUserFromFeed.add(req.fromUserId.toString());
+            hideUserFromFeed.add(req.toUserId.toString());
+        }) 
+
+       const user = await User.find({
+        $and :[{ _id : {$nin : Array.from(hideUserFromFeed)}},{_id : {$ne : loggedInUser._id}}]
+       
+       
+       }).select("firstName lastName photoUrl gender age skills").skip(skip).limit(limit)
+       res.send(user);
+
+
+
+    } catch (error) {
+        res.status(400).send({err: error.message})
     }
 })
 
